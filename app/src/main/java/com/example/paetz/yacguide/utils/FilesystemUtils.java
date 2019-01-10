65b2cb72cec6f506034519ce68e2a463a544b8d2
Axel Paetzold <paetz@Axels-MBP.3amlabs.net>
2019-01-10 12:05:21 +0100

    fixed storage permission problem

--- app/src/main/java/com/example/paetz/yacguide/utils/FilesystemUtils.java
+++ app/src/main/java/com/example/paetz/yacguide/utils/FilesystemUtils.java
@@ -4,15 +4,8 @@ import android.os.Environment;
 
 public class FilesystemUtils {
 
-    public static boolean isExternalStorageWritable() {
+    public static boolean isExternalStorageAvailable() {
         String state = Environment.getExternalStorageState();
         return Environment.MEDIA_MOUNTED.equals(state);
     }
-
-    public static boolean isExternalStorageReadable() {
-        String state = Environment.getExternalStorageState();
-        return (Environment.MEDIA_MOUNTED.equals(state)
-             || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state));
-    }
-
 }
