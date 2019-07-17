1aac9d1eb7edb6c927212221434238e7f1e0324f
Fabian Kantereit <fabian.kantereit@tsaro.io>
2019-07-17 20:05:28 +0200

    Convert Partner and PartnerDao to Kotlin
    Signed-off-by: Fabian Kantereit <fabian.kantereit@tsaro.io>

---
+++ app/src/main/java/com/example/paetz/yacguide/database/Partner.kt
@@ -0,0 +1,14 @@
+package com.example.paetz.yacguide.database
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
+class Partner {
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+
+    var name: String? = null
+
+}
