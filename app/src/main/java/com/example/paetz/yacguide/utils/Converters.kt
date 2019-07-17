11b48fc0e003227326df8c1674f19d02c89c076b
Fabian Kantereit <fabian.kantereit@tsaro.io>
2019-07-17 20:49:42 +0200

    Convert Converters to Kotlin
    Signed-off-by: Fabian Kantereit <fabian.kantereit@tsaro.io>

---
+++ app/src/main/java/com/example/paetz/yacguide/utils/Converters.kt
@@ -0,0 +1,25 @@
+package com.example.paetz.yacguide.utils
+
+import android.arch.persistence.room.TypeConverter
+import android.text.TextUtils
+import java.util.ArrayList
+
+class Converters {
+    @TypeConverter
+    fun fromString(intListAsString: String): ArrayList<Int> {
+        val intList = ArrayList<Int>()
+        if (!intListAsString.isEmpty()) {
+            val strList = intListAsString.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            for (str in strList) {
+                intList.add(Integer.parseInt(str.trim { it <= ' ' }))
+            }
+        }
+        return intList
+    }
+
+    @TypeConverter
+    fun fromIntList(intList: ArrayList<Int>): String {
+        return TextUtils.join(",", intList)
+    }
+
+}
