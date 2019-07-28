89e1b8e79288f8a7aa41f4a8785661ea58fd952d
Fabian Kantereit <fabian.kantereit@tsaro.io>
2019-07-28 15:53:19 +0200

    Cleanup NetworkUtils
    Signed-off-by: Fabian Kantereit <fabian.kantereit@tsaro.io>

--- app/src/main/java/com/example/paetz/yacguide/utils/NetworkUtils.kt
+++ app/src/main/java/com/example/paetz/yacguide/utils/NetworkUtils.kt
@@ -2,36 +2,19 @@ package com.example.paetz.yacguide.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.Uri
+import java.net.URLEncoder
 
 import java.util.HashMap
 
 object NetworkUtils {
-
-    // I couldn't find a proper java library, so let's escape it on our own
-    private val URL_ENCODE_MAP = object : HashMap<String, String>() {
-        init {
-            put(" ", "%20")
-            put("Ä", "%C4")
-            put("ä", "%E4")
-            put("Ö", "%D6")
-            put("ö", "%F6")
-            put("Ü", "%DC")
-            put("ü", "%FC")
-            put("ß", "%DF")
-        }
-    }
-
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetworkInfo = connectivityManager.activeNetworkInfo
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+        return activeNetworkInfo?.isConnected ?: false
     }
 
     fun encodeString2Url(str: String): String {
-        var result = str
-        for ((key, value) in URL_ENCODE_MAP) {
-            result = result.replace(key, value)
-        }
-        return result
+        return URLEncoder.encode(str, "UTF-8")
     }
 }
