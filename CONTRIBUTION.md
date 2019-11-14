74d8681028cf3639542fbe5ba8748040eced4083
Christian Sommer <chrger.noe@gmail.com>
2019-11-14 01:27:07 +0100

    Travis CI Integration (#71)

---
+++ CONTRIBUTION.md
@@ -0,0 +1,25 @@
+## Building the Docker Image
+
+**This step is only required, if you work at the build environment!**
+
+Build the Docker image inside the project root directory.
+
+```shell
+docker build -f docker/Dockerfile -t yacgroup/yacguide-build .
+```
+
+## Building a APK locally
+
+Run the following commands within the repository root.
+
+Copy the `keystore.jks` file from your private location to
+the project directory but **DO NOT** commit this file.
+
+Run the build inside the container:
+
+```shell
+docker/build.sh <storepass> <keypass>
+```
+
+The parameters `<storepass>` and `<keypass>` are the keystore and key
+passwords for signing the APK file.
---
+++ docker/build.sh
@@ -0,0 +1,78 @@
+#! /usr/bin/env bash
+
+storepass="$1"
+keypass="$2"
+if [ -z ${3+x} ]; then
+    runDocker=true
+fi
+
+dockerImage="yacgroup/yacguide-build"
+dockerContainer="yacguide-build"
+dockerMountTarget="/mnt/yacguide"
+releaseDir="$(pwd)/app/build/outputs/apk/release"
+releaseApk="${releaseDir}/yacguide.apk"
+userId=$(id --user)
+groupId=$(id --group)
+userName=$(id --user --name)
+groupName=$(id --group --name)
+
+if [ ${runDocker} ]; then
+    ### Run this script in the Docker container context
+    # Start new container in background and mount local Git repository
+    # into the container.
+    echo "Starting container ..."
+    docker run \
+        --detach \
+        --rm \
+        --name ${dockerContainer} \
+        --mount type=bind,src="$(pwd)",dst="${dockerMountTarget}" \
+        ${dockerImage} /bin/bash -c "tail -f /dev/null"
+    # Create user inside the container with the same UID and GID as
+    # the current user. This makes sure that the files and directories
+    # of local Git repository are modified with the same permissions.
+    echo "Creating build user inside the container ..."
+    groupaddCmd="groupadd --gid ${groupId} ${groupName}"
+    useraddCmd="useradd \
+                    --create-home \
+                    --uid ${userId} \
+                    --gid ${groupId} \
+                    ${userName}"
+    docker exec \
+        ${dockerContainer} \
+        /bin/bash -c "${groupaddCmd} && ${useraddCmd}"
+    # Run the build script inside the container as the previously
+    # created user.
+    echo "Running the actual build inside the container ..."
+    docker exec \
+        --user ${userId}:${groupId} \
+        --workdir "${dockerMountTarget}" \
+        ${dockerContainer} \
+        $0 $storepass $keypass false
+    echo "Stopping container ..."
+    docker stop --time 0 ${dockerContainer}
+else
+    echo "Building unsigned APK ..."
+    ./gradlew \
+        --gradle-user-home $(pwd)/.gradle/ \
+        clean \
+        assembleRelease
+
+    echo "Signing APK ..."
+    jarsigner \
+        -verbose \
+        -keystore keystore.jks \
+        -storepass "$storepass" \
+        -keypass "$keypass" \
+        -signedjar ${releaseDir}/app-release-signed.apk \
+        ${releaseDir}/app-release-unsigned.apk \
+        key0
+
+    # https://developer.android.com/studio/command-line/zipalign
+    echo "ZIP alignment ..."
+    ${ANDROID_HOME}/build-tools/${ANDROID_BUILD_TOOLS}/zipalign \
+        -f \
+        4 \
+        ${releaseDir}/app-release-signed.apk \
+        ${releaseApk}
+    echo "APK file: ${releaseApk}"
+fi
