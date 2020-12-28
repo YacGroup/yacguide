93be852cd349bde5d2fe0f6c560b1af547b1bad3
Christian Sommer <chrger.noe@gmail.com>
2020-12-28 20:42:31 +0100

    Rework of about view
    * Use default background (white)
    * Left align items
    * Make items with URLs clickable

--- app/src/main/java/com/yacgroup/yacguide/AboutActivity.kt
+++ app/src/main/java/com/yacgroup/yacguide/AboutActivity.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2020 Axel Paetzold
+ *               2020 Christian Sommer
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,8 +22,9 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
+import android.webkit.URLUtil
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 
@@ -36,12 +38,33 @@ class AboutActivity : BaseNavigationActivity() {
         super.onCreate(savedInstanceState)
         this.title = getString(R.string.menu_about, getString(R.string.app_name))
 
-        val textViewAppVersion: TextView = findViewById(R.id.textViewAppVersion)
-        textViewAppVersion.text = "Version ${getAppVersion()}"
-        textViewAppVersion.gravity = Gravity.CENTER_HORIZONTAL
+        _createEntry(getString(R.string.data_src), getString(R.string.data_src_url),null)
+        _createEntry(getString(R.string.privacy_policy), null) { _showPrivacyPolicy() }
+        _createEntry(
+                getString(R.string.source_code_and_support),
+                getString(R.string.github_url),
+                null )
+        _createEntry(getString(R.string.license), getString(R.string.license_url),null)
+        _createEntry(getString(R.string.version), _getAppVersion(), null)
     }
 
-    private fun getAppVersion() : String {
+    private fun _createEntry(
+            title: String,
+            description: String?,
+            callback: ((view: View) -> Unit)?) {
+        val contentAbout: LinearLayout = findViewById(R.id.aboutContent)
+        val entry = layoutInflater.inflate(R.layout.about_entry, null)
+        if (callback == null && URLUtil.isValidUrl(description)) {
+            entry.setOnClickListener { description?.let { url -> _openUrl(url) } }
+        } else {
+            entry.setOnClickListener(callback)
+        }
+        entry.findViewById<TextView>(R.id.aboutEntryTitle).text = title
+        entry.findViewById<TextView>(R.id.aboutEntryDescription).text = description
+        contentAbout.addView(entry)
+    }
+
+    private fun _getAppVersion(): String {
         try {
             val pkgManager = this.packageManager
             val pkgInfo = pkgManager.getPackageInfo(this.packageName, 0)
@@ -52,16 +75,14 @@ class AboutActivity : BaseNavigationActivity() {
         return "0.0"
     }
 
-    @Suppress("UNUSED_PARAMETER")
-    fun showPrivacyPolicy(v: View) {
+    private fun _showPrivacyPolicy() {
         val intent = Intent(this, PrivacyPolicyActivity::class.java)
         startActivity(intent)
     }
 
-    @Suppress("UNUSED_PARAMETER")
-    fun showLicense(v: View) {
+    private fun _openUrl(url: String) {
         val openURL = Intent(android.content.Intent.ACTION_VIEW)
-        openURL.data = Uri.parse(getString(R.string.license_url))
+        openURL.data = Uri.parse(url)
         if (openURL.resolveActivity(packageManager) != null) {
             startActivity(openURL)
         } else {
