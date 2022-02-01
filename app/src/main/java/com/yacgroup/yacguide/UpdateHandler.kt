0458d87b71905c5c52d48e1e19702eb1583d7d3c
paetz <paetz89@yahoo.de>
2022-02-01 10:54:56 +0100

    Review changes 2

--- app/src/main/java/com/yacgroup/yacguide/UpdateHandler.kt
+++ app/src/main/java/com/yacgroup/yacguide/UpdateHandler.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Axel Paetzold
+ * Copyright (C) 2021, 2022 Axel Paetzold
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
