6cdbac3fdde222cfb9a29fc151b9e08b1405a09d
paetz <45095788+paetz@users.noreply.github.com>
2022-02-24 11:37:21 +0100

    Added region dao unit tests (#330)

--- app/src/main/java/com/yacgroup/yacguide/database/RegionDao.kt
+++ app/src/main/java/com/yacgroup/yacguide/database/RegionDao.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Fabian Kantereit
+ * Copyright (C) 2019, 2022 Axel Paetzold
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
