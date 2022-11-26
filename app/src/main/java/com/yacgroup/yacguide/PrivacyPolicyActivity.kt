3a028975adc0f1ad3ebbe0379327e6941cbe075a
paetz <paetz89@yahoo.de>
2022-11-26 22:36:15 +0100

    Review changes

--- app/src/main/java/com/yacgroup/yacguide/PrivacyPolicyActivity.kt
+++ app/src/main/java/com/yacgroup/yacguide/PrivacyPolicyActivity.kt
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2020 Christian Sommer,
- *               2022 Axel Pätzold
+ *               2022 Axel Paetzold
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
