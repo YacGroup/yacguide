5a2cc8c1a8eba7e8388d9efcf2b2b95a69784638
paetz <paetz89@yahoo.de>
2023-01-16 14:05:51 +0100

    Migrated comments layout to xml

---
+++ app/src/main/java/com/yacgroup/yacguide/utils/CommentUtils.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2023 Axel Paetzold
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.yacgroup.yacguide.utils
+
+class Comment(
+    val text: String,
+    val properties: List<CommentProperty>
+)
+
+class CommentProperty(
+    val layoutResource: Int,
+    val nameResource: Int,
+    val qualityMap: Map<Int, String>,
+    val qualityId: Int
+)
